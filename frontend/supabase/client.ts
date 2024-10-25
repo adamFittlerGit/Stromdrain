43389da8cd3396a4709c68fbbb22930b5bbadd4b
adam <adam@LAPTOP-D52M9NN4>
2024-10-25 13:46:27 +1000

    Fixed supabase vunerability

--- frontend/supabase/client.ts
+++ frontend/supabase/client.ts
@@ -9,4 +9,4 @@ const supabaseUrl: string = process.env.SUPABASE_URL!;
 const supabaseKey: string = process.env.SUPABASE_MASTER_KEY!;
 
 // Update this to not use direct keys on the frontend, we need to move the backend logic to an api route on the api folder in order to make this possible, the frontend will not have access to these environment vairables
-export const supabaseClient = createClient("https://jzlzzjcinqypqardzzfq.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6bHp6amNpbnF5cHFhcmR6emZxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyOTYwMTk1NSwiZXhwIjoyMDQ1MTc3OTU1fQ.E_Rl9yfXiZPWXx-v5PKy8nSq1fsDilwc8EijkIqwGN4")
+export const supabaseClient = createClient(supabaseUrl, supabaseKey)
