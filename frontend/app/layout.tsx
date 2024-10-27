e16cb801beba3048bc2553184ceaea296b131081
adam <adam@LAPTOP-D52M9NN4>
2024-10-27 17:52:17 +1000

    Cross browser explored chrome fixed

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#337aff]` }
       >
         <TopNav/>
-        {children}
+          {children}
       </body>
     </html>
   );
