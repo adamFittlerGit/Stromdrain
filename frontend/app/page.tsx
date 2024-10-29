1db43ae468c818cd50356f1d3c147a1a167862ef
adam <adam@LAPTOP-D52M9NN4>
2024-10-29 16:03:28 +1000

    Adding react atom 3D animation by copying and pasting threejs code will actually do somehting original in future but its cool for now

--- frontend/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Home = () => {
-  return (
-    <div>Add a little explaination and legend here in order to explain what the sit is about</div>
-  )
-}
-
-export default Home
