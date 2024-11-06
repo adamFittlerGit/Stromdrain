3edf7644e85a7ebe141fc11cbf28da9c5bb4b156
adam <adam@LAPTOP-D52M9NN4>
2024-11-06 15:29:05 +1000

    Query UI cleaned

--- frontend/app/stormai/page.tsx
+++ frontend/app/stormai/page.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 import { TypeAnimation } from 'react-type-animation';
 
 export default function Home() {
-  const [query, setQuery] = useState("");
+  const [queries, setQueries] = useState("");
   const [response, setResponse] = useState("");
 
   async function getResponse() {
@@ -24,11 +24,11 @@ export default function Home() {
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
-  }, [query]); // Include query dependency to use the latest query when enter is pressed
+  }, [queries]); // Include query dependency to use the latest query when enter is pressed
 
   return (
-    <div  className="flex items-center justify-center mt-10">
-        <main className="flex flex-col gap-8 row-start-2 items-center w-3/4 sm:w-128">
+    <div className={`flex ${response ? "items-center" : ""} justify-center mt-10 h-screen pb-20`}>
+        <main className="flex items-end flex-col gap-8 row-start-2 items-center w-3/4 sm:w-128">
           {response && (
               <div className="flex items-start">
                 <Image
@@ -76,8 +76,8 @@ export default function Home() {
           >
             <div className="grid grid-cols-8">
               <input
-                value={query}
-                onChange={(e) => setQuery(e.target.value)} // Update query as user types
+                value={queries[-1]}
+                onChange={(e) => setQueries(e.target.value)} // Update query as user types
                 placeholder="What would you like to learn?"
                 className="col-span-8 sm:col-span-7 bg-gray-200 rounded-lg p-3 border-2 border-black"
               />
