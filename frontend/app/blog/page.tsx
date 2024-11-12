ca3275af016dfd88739f90e6ad592b8132e74d42
adamFittlerGit <fittleradam@gmail.com>
2024-11-12 20:23:28 +1000

    UI Bug fixed

--- frontend/app/blog/page.tsx
+++ frontend/app/blog/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
   const [tagType, setTagType] = useState("all");
   const [page, setPage] = useState(1); // Current page state
   const [query, setQuery] = useState("");
-  const postsPerPage = 10; // Number of posts per page
+  const postsPerPage = 10; // Number of posts per page]
   const skeletons = [];
 
   for (let i = 0; i < 10; i++) {
@@ -86,6 +86,50 @@ export default function Home() {
     <div className="flex justify-center">
       <div className="w-3/4">
         <h1 className="text-5xl font-bold pt-6 pb-12 text-center text-white">MY BLOG</h1>
+
+        {/* This is for the AI Search and RAG Feature*/}
+        <div className="hidden flex justify-center items-center my-4">
+          <Image
+              src="/storm-ai.png" // path to the image in the public folder
+              alt="Storm AI Logo"
+              width={50} // Adjust the width
+              height={50} // Adjust the height
+              className="mr-4"
+          />
+          <p className="pr-4 text-3xl fold-bold text-black">:</p>
+          <input
+            value={query}
+            onChange={(e) => {
+              setQuery(e.target.value)  
+              console.log(query)
+            }} // Update query as user types
+            placeholder="Search for a post!"
+            className="bg-white rounded-lg p-3 border-2 border-black w-2/3 sm:w-96 hover:border-sky-400"
+          />
+        </div>
+
+        {/* New Post Section */}
+        {isLoggedIn && 
+        <div className="flex justify-center">
+          <Tilt className="w-64">
+              <div  className="flex justify-center items-center col-span-1 p-4 m-4 bg-white rounded-lg border-2 border-black h-5/6">
+                <Link href="/new"> 
+                  <div className="flex justify-center">
+                    <Image
+                      className="p-2"
+                      src="/write.png"
+                      width={100}
+                      height={100}
+                      alt="Post Image"
+                    />
+                  </div>
+                    <h1 className="text-lg font-bold text-center text-black">New Post</h1>
+                </Link>
+              </div>
+            </Tilt>
+          </div>
+        }
+
         {/* Pagination Controls */}
         <div className="flex justify-center items-center mt-6 space-x-4">
           <button
@@ -104,7 +148,25 @@ export default function Home() {
             Next
           </button>
         </div>
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+
+        {/* Currently hidden tag selector*/}
+        <div className="hidden flex justify-center text-center">
+            <select className="mx-1 rounded p-1 text-center text-black" id="tags" name="tags" onChange={(e) => {
+              setTagType(e.target.value)
+            }}>
+              <option className="text-center text-black" value="all" selected>All Tags</option>
+              <option className="text-center text-black" value="university">University</option>
+              <option className="text-center text-black" value="software-engineering">Software Engineering</option>
+              <option className="text-center text-black" value="project-progress">Project Progress</option>
+              <option className="text-center text-black" value="fitness">Fitness</option>
+              <option className="text-center text-black" value="martial-arts">Martial Arts</option>
+              <option className="text-center text-black" value="general-learning">General</option>
+              <option className="text-center text-black" value="thoughts">Thoughts</option>
+            </select>
+        </div>
+
+        {/* Section for the blog posts */}
+        <div className="grid  sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
           {isMounted
             ? paginatedPosts.map((postProps:any, index) => (
                 <Tilt key={index}>
