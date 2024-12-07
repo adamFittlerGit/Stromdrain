f604c0d2e9d7b20900beaa2d876279523ce3ed03
adamFittlerGit <fittleradam@gmail.com>
2024-12-08 09:50:30 +1000

    backend setup for updating and deleting and routes for updating added

--- frontend/app/api/updatePost/route.ts
+++ frontend/app/api/updatePost/route.ts
@@ -1,7 +1,14 @@
-import { NextRequest, NextResponse } from "next/server" 
+import { NextRequest, NextResponse } from "next/server";
+import { supabaseClient } from '@/supabase/client';
 
 export async function POST(res: NextRequest) {
-  const data = await res.json()
+  const {id, title, body} = await res.json()
   
+  const { data } = await supabaseClient.rpc('update_post', {
+    update_id: id,
+    new_title: title, // Choose an appropriate threshold for your data
+    new_body: body, // Choose the number of matches
+  })
+
   return NextResponse.json({ data })
 }
