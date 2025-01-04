58e44b3438f2d8c6a2adf912e2e569302837ac26
adamFittlerGit <fittleradam@gmail.com>
2025-01-05 09:06:52 +1000

    disabled middleware to make site work

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -5,18 +5,18 @@ import { supabaseClient } from '@/supabase/client';
 export async function middleware(request: NextRequest) {
   
   // Define the path that needs authentication
-  const protectedPaths = ['/blog/new', '/stormai'];
+  //const protectedPaths = ['/blog/new', '/stormai'];
 
-  const isProtectedPath = protectedPaths.some(path => path === request.nextUrl.pathname); // Check if requested path is in that list .some returns true if at least one item in list equalates to true with this 
+  //const isProtectedPath = protectedPaths.some(path => path === request.nextUrl.pathname); // Check if requested path is in that list .some returns true if at least one item in list equalates to true with this 
 
-  const { data, error } = await supabaseClient.auth.getSession()
+  //const { data, error } = await supabaseClient.auth.getSession()
 
-  if (isProtectedPath) {
-    if (!error) {
+  //if (isProtectedPath) {
+   // if (!error) {
       // Redirect to login if there's no token
-      return NextResponse.redirect(new URL('/login', request.url));
-    } else {
+   //   return NextResponse.redirect(new URL('/login', request.url));
+  //  } else {
       return NextResponse.next();
-    }
-  }
+   // }
+ // }
 }
