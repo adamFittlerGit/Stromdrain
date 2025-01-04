d5e6754a96ed9f6775f16a98a5cd228df1cabeea
adamFittlerGit <fittleradam@gmail.com>
2025-01-05 00:04:02 +1000

    middleware upadted for supabase auth

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,39 +1,22 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { jwtVerify } from 'jose'
+import { supabaseClient } from '@/supabase/client';
 
 export async function middleware(request: NextRequest) {
-  const token = request.cookies.get('auth_token')?.value;
-
-  // Define the secret from your environment variables
-  const jwtSecret = process.env.JWT_SECRET;
-
-  const encodedKey = new TextEncoder().encode(jwtSecret)
-
+  
   // Define the path that needs authentication
-  const protectedPaths = ['/blog/new', '/stormai', '/habits'];
+  const protectedPaths = ['/blog/new', '/stormai'];
 
   const isProtectedPath = protectedPaths.some(path => path === request.nextUrl.pathname); // Check if requested path is in that list .some returns true if at least one item in list equalates to true with this 
 
+  const { data, error } = await supabaseClient.auth.getSession()
 
   if (isProtectedPath) {
-    if (!token) {
+    if (!error) {
       // Redirect to login if there's no token
       return NextResponse.redirect(new URL('/login', request.url));
-    }
-
-    try {
-      const { payload } = await jwtVerify(token, encodedKey, {
-        algorithms: ['HS256'],
-      })
-
-      if (payload.username === "Stormed") {
-        return NextResponse.next();
-      }
-    } catch (error) {
-      console.log(error)
-      return NextResponse.redirect(new URL('/login', request.url));
+    } else {
+      return NextResponse.next();
     }
   }
-
 }
