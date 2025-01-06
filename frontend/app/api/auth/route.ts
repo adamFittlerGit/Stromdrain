49881893fbc8061dd9cd371e8957ec5ee683da3f
adamFittlerGit <fittleradam@gmail.com>
2025-01-06 21:37:48 +1000

    auth somewhat fixed need to add cookie based auth as well

--- frontend/app/api/auth/route.ts
+++ frontend/app/api/auth/route.ts
@@ -5,12 +5,12 @@ import type { NextRequest } from 'next/server';
 export async function GET(request: NextRequest) {
     
     const { data, error } = await supabaseClient.auth.getSession()
-
-    if (!error) {
-        return NextResponse.json({ loggedIn: true })
+    console.log(`supabase.auth:${data.session}`)
+    if (data.session === null) {
+        return NextResponse.json({ loggedIn: false })
         
     }  else{
-        return NextResponse.json({ loggedIn: false });
+        return NextResponse.json({ loggedIn: true });
     }
 
 }
