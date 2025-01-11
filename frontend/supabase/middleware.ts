fab2a610bfa9604e495e71b1e4e57a902513a904
adamFittlerGit <fittleradam@gmail.com>
2025-01-11 23:27:11 +1000

    middleware protection added properly now

--- frontend/supabase/middleware.ts
+++ frontend/supabase/middleware.ts
@@ -37,16 +37,19 @@ export async function updateSession(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser()
 
+ 
   if (
     !user &&
-    !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/auth')
+    request.nextUrl.pathname.startsWith('/blog/new') ||
+    request.nextUrl.pathname.startsWith('/stormai')
   ) {
     // no user, potentially respond by redirecting the user to the login page
-    //const url = request.nextUrl.clone()
-    //url.pathname = '/login'
-    //return NextResponse.redirect(url)
-  }
+    const url = request.nextUrl.clone()
+    url.pathname = '/login'
+    return NextResponse.redirect(url)
+  } 
+
+
 
   // IMPORTANT: You *must* return the supabaseResponse object as it is.
   // If you're creating a new response object with NextResponse.next() make sure to:
