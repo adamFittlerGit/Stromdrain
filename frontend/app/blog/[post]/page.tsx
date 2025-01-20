e19a4b9633121a1f15f5e982724de7d15483f2de
adamFittlerGit <fittleradam@gmail.com>
2025-01-21 06:24:15 +1000

    All video posts start muted

--- frontend/app/blog/[post]/page.tsx
+++ frontend/app/blog/[post]/page.tsx
@@ -152,7 +152,7 @@ const Post = () => {
                   height={200}
               />
               ) : (
-                <video width="320" height="240" controls>
+                <video width="320" height="240" controls muted>
                   <source src={post?.image_urls[0]} />
                   Your browser does not support the video tag.
                 </video>
