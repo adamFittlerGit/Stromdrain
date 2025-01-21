7271cbc7cad2017f19dd47c7db49d655c81ded33
adamFittlerGit <137750965+adamFittlerGit@users.noreply.github.com>
2025-01-21 12:38:53 +1000

    Update page.tsx
    fixed margin

--- frontend/app/blog/[post]/page.tsx
+++ frontend/app/blog/[post]/page.tsx
@@ -119,7 +119,7 @@ const Post = () => {
   return (
     <div className="flex justify-center text-black">
       {isMounted && params?.post ? (// create a post type to remove typescript error
-        <div className="grid p-4 m-4 bg-white border-2 border-black rounded  w-3/4 sm:w-128 ">
+        <div className="grid p-4 my-4 bg-white border-2 border-black rounded  w-3/4 sm:w-128 ">
           <div className="hidden flex justify-start absolute z-50 opacity-50">
             <button className={`border-2 border-black rounded-full p-1 bg-gray-300 ${!showSummary ? "hover:bg-sky-400": ""}`}>
               <Image
