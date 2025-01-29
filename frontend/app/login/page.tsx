447859b66f2641b94c03d8532c6ce1b6cab1247a
adamFittlerGit <137750965+adamFittlerGit@users.noreply.github.com>
2025-01-29 10:07:05 +1000

    Update page.tsx

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,11 +1,11 @@
-'use client'
-import { useState } from 'react';
-import { login, signup } from './actions'
+'use client';
+import { useState, FormEvent } from 'react';
+import { login } from './actions';
 
 export default function LoginPage() {
-  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
 
-  async function handleSubmit(event) {
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
     setIsSubmitting(true);
     const formData = new FormData(event.currentTarget);
@@ -20,7 +20,9 @@ export default function LoginPage() {
     <div className="flex m-6 justify-center">
       <form onSubmit={handleSubmit} className="bg-white p-6 rounded-lg shadow-md w-full max-w-sm">
         <h2 className="text-black text-2xl font-bold mb-4 text-center">Login</h2>
-        <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email:</label>
+        <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+          Email:
+        </label>
         <input
           id="email"
           name="email"
@@ -29,7 +31,9 @@ export default function LoginPage() {
           disabled={isSubmitting}
           className="text-black mt-1 mb-4 p-2 w-full border border-gray-300 rounded-md disabled:opacity-50"
         />
-        <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password:</label>
+        <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+          Password:
+        </label>
         <input
           id="password"
           name="password"
