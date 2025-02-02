d8aa42f9a830ccfb064c2f2ee3c1e0183fb6f8f4
adamFittlerGit <fittleradam@gmail.com>
2025-02-02 23:59:58 +1000

    f

--- frontend/app/stormai/page.tsx
+++ frontend/app/stormai/page.tsx
@@ -6,9 +6,11 @@ import { TypeAnimation } from 'react-type-animation';
 export default function Home() {
   const [query, setQuery] = useState("");
   const [response, setResponse] = useState("");
+  const [loading, setLoading] = useState(false);
 
   // Move async function outside of component
   const getResponse = async () => {
+    setLoading(true);
     const res = await fetch("/api/openai", {
       method: "POST",
       headers: {
@@ -23,6 +25,7 @@ export default function Home() {
     
     const data = await res.json();
     setResponse(data)
+    setLoading(false);
     return data;
   }
 
@@ -94,11 +97,11 @@ export default function Home() {
                 value={query}
                 onChange={(e) => setQuery(e.target.value)} // Update query as user types
                 placeholder="Ask me anything?"
-                className="bg-white rounded-lg p-3 border-2 border-black w-2/3 sm:w-96 hover:border-sky-400"
-                disabled={!!response}
+                className={`${(query && !loading) && "hover:border-sky-400"}bg-white rounded-lg p-3 border-2 border-black w-2/3 sm:w-96 `}
+                disabled={!!response || loading}
               />
               {!response ?
-                <button type="submit" onClick={() => getResponse()} disabled={!query || !!response} className={`${query && "hover:bg-sky-400"} bg-gray-500 ml-2 p-3 text-white h-full rounded-lg border-black border-2`} >
+                <button type="submit" onClick={() => getResponse()} disabled={!query || !!response || loading} className={`${(query && !loading) && "hover:bg-sky-400"} bg-gray-500 ml-2 p-3 text-white h-full rounded-lg border-black border-2`} >
                   Ask
                 </button>
               :
